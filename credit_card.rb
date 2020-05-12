73c65071f67ae10ca7e4253829ce8111fd5a8cbc
david <david@Jussis-MacBook-Air.local>
2020-05-12 19:42:09 +0200

    Upgrading the tests

--- credit_card.rb
+++ credit_card.rb
@@ -7,25 +7,62 @@ require 'thor' # gem install thor; if you dont have installed
 # For Help:
 #       ruby credit_card.rb 
 
+module ModuleCreditCardHelper
+    # Calculates the sum the credit card number
+    def self.check_sum(number)
+        sum = 0
+        number.reverse.split("").each_with_index do |num, i|
+            if((i+1)%2 == 0)
+                aux = (num.to_i*2).to_s
+                aux.length > 1 ? sum += aux[0].to_i+aux[1].to_i : sum+=aux.to_i
+            else
+                sum+=num.to_i
+            end
+        end
+        sum %10 == 0 ? "valid" : "invalid"
+    end
+
+    # Check the type of the credit card number
+    def self.check_type(number)
+        num2 = number.slice(0,2)
+        if (num2 == '34' || num2 == '37') && number.length == 15
+            return {status: :success, type: "AMEX"}
+        elsif [*51..55].include?(num2.to_i) && number.length == 16
+            return {status: :success, type: "MasterCard"}
+        elsif number.slice(0,4) == '6011' && number.length == 16
+            return {status: :success, type: "Discover"}
+        elsif number[0] == '4' && (number.length == 13 || number.length == 16)
+            return {status: :success, type: "VISA"}
+        end
+        {status: :error, type: "Unknown"}
+    end 
+end
+
 class CreditCard < Thor
+    include ModuleCreditCardHelper
     class_option :verbose, :type => :boolean, :aliases => "-v"
 
     desc "validate CREDIT_CARD_NUMBER", "Check if the number of the credit card it's valid"
     def validate(credit_card_number)
-        # Make sure that credit card number only contains numbers and spaces
-        return puts "Error of Character, the Credit Card number has to only contains NUMBERS" if credit_card_number.to_s.match(/\D.\s/)
         # In case that the credit card contains spaces, this removes the spaces
         number = credit_card_number.split(" ").join("")
+        
+        # Make sure that credit card number only contains numbers
+        if number.to_s.match(/\D/) 
+            puts "Error of Character, the Credit Card number has to only contains NUMBERS" 
+            return "Error of Character"
+        end
+        
         credit_card = { type: "",  number: number, validation: ""}
         
-        getting_type = check_type(number)
+        getting_type = ModuleCreditCardHelper.check_type(number)
         case getting_type[:status]
         when :error 
-            puts "Unknown: #{number} (invalid)"
-            return "Unknown: #{number} (invalid)"
+            puts "#{getting_type[:type]}: #{number} (invalid)"
+            return "#{getting_type[:type]}: #{number} (invalid)"
         when :success
             credit_card[:type] = getting_type[:type]
-            credit_card[:validation] = check_sum(number)
+            credit_card[:validation] = ModuleCreditCardHelper.check_sum(number)
             puts "#{credit_card[:type]}: #{credit_card[:number]} (#{credit_card[:validation]})"
             return "#{credit_card[:type]}: #{credit_card[:number]} (#{credit_card[:validation]})"
         end
@@ -34,35 +71,6 @@ class CreditCard < Thor
     def self.exit_on_failure?
         puts "Command not found. Please check the syntax"
     end
-end
-
-# Calculates the sum the credit card number
-def check_sum(number)
-    sum = 0
-    number.reverse.split("").each_with_index do |num, i|
-        if((i+1)%2 == 0)
-            aux = (num.to_i*2).to_s
-            aux.length > 1 ? sum += aux[0].to_i+aux[1].to_i : sum+=aux.to_i
-        else
-            sum+=num.to_i
-        end
-    end
-    sum %10 == 0 ? "valid" : "invalid"
-end
-
-# Check the type of the credit card number
-def check_type(number)
-    num2 = number.slice(0,2)
-    if (num2 == '34' || num2 == '37') && number.length == 15
-        return {status: :success, type: "AMEX"}
-    elsif [*51..55].include?(num2.to_i) && number.length == 16
-        return {status: :success, type: "MasterCard"}
-    elsif number.slice(0,4) == '6011' && number.length == 16
-        return {status: :success, type: "Discover"}
-    elsif number[0] == '4' && (number.length == 13 || number.length == 16)
-        return {status: :success, type: "VISA"}
-    end
-    {status: :error}
 end
 
 CreditCard.start(ARGV)
--- credit_card_test.rb
+++ credit_card_test.rb
@@ -2,6 +2,56 @@ require "test/unit"
 require_relative './credit_card'
 
 class CreditCardTest < Test::Unit::TestCase
+    include ModuleCreditCardHelper
+
+    # Check types of credit cards
+    def test_types
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('4111111111111111').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('4111111111111111')[:status])
+        assert_equal("VISA", ModuleCreditCardHelper.check_type('4111111111111111')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('4111111111111').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('4111111111111')[:status])
+        assert_equal("VISA", ModuleCreditCardHelper.check_type('4111111111111')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('4012888888881881').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('4012888888881881')[:status])
+        assert_equal("VISA", ModuleCreditCardHelper.check_type('4012888888881881')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('378282246310005').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('378282246310005')[:status])
+        assert_equal("AMEX", ModuleCreditCardHelper.check_type('378282246310005')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('6011111111111117').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('6011111111111117')[:status])
+        assert_equal("Discover", ModuleCreditCardHelper.check_type('6011111111111117')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('9111111111111111').class)
+        assert_equal(:error, ModuleCreditCardHelper.check_type('9111111111111111')[:status])
+        assert_equal("Unknown", ModuleCreditCardHelper.check_type('9111111111111111')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('5105105105105106').class)
+        assert_equal(:success, ModuleCreditCardHelper.check_type('5105105105105106')[:status])
+        assert_equal("MasterCard", ModuleCreditCardHelper.check_type('5105105105105106')[:type])
+
+        assert_equal(Hash, ModuleCreditCardHelper.check_type('5105 1051 0510 5106').class)
+        assert_equal(:error, ModuleCreditCardHelper.check_type('5105 1051 0510 5106')[:status])
+        assert_equal("Unknown", ModuleCreditCardHelper.check_type('5105 1051 0510 5106')[:type])
+    end
+
+    # Check the sum of numbers cards
+    def test_numbers
+        assert_equal("valid", ModuleCreditCardHelper.check_sum('4111111111111111'))
+        assert_equal("invalid", ModuleCreditCardHelper.check_sum('9111111111111111'))
+        assert_equal("valid", ModuleCreditCardHelper.check_sum('911111111111111'))
+        assert_equal("valid", ModuleCreditCardHelper.check_sum('378282246310005'))
+        assert_equal("valid", ModuleCreditCardHelper.check_sum('5105105105105100'))
+        assert_equal("invalid", ModuleCreditCardHelper.check_sum('5105 1051 0510 5106'))
+        assert_equal("invalid", ModuleCreditCardHelper.check_sum('5105 1051 0510 5100'))
+        assert_equal("invalid", ModuleCreditCardHelper.check_sum('5105105105105106'))
+    end
+
+    # Check validation from some cards
     def test_cards
         assert_equal('VISA: 4111111111111111 (valid)', CreditCard.new().validate('4111111111111111') )
         assert_equal('VISA: 4111111111111 (invalid)', CreditCard.new().validate('4111111111111') )
@@ -11,5 +61,15 @@ class CreditCardTest < Test::Unit::TestCase
         assert_equal('MasterCard: 5105105105105100 (valid)', CreditCard.new().validate('5105105105105100') )
         assert_equal('MasterCard: 5105105105105106 (invalid)', CreditCard.new().validate('5105 1051 0510 5106') )
         assert_equal('Unknown: 9111111111111111 (invalid)', CreditCard.new().validate('9111111111111111') )
+
+        assert_equal('MasterCard: 5155327746759207 (valid)', CreditCard.new().validate('5155327746759207') )
+        assert_equal('Discover: 6011951670514932 (valid)', CreditCard.new().validate('6011951670514932') )
+        assert_equal('Unknown: 515527746759207 (invalid)', CreditCard.new().validate('515527746759207') )
+        
+        assert_equal('Error of Character', CreditCard.new().validate('911111eqweweeqw1111111111') )
+        assert_equal('Unknown:  (invalid)', CreditCard.new().validate('') )
+        assert_equal('Error of Character', CreditCard.new().validate('312;3"11232-30eqwe09qw8093812') )
+        assert_equal('AMEX: 378282246310005 (valid)', CreditCard.new().validate('3782    82246310005') )
+        assert_equal('Error of Character', CreditCard.new().validate('ddjoidj1o2ij312j312oi3') )
     end
 end
